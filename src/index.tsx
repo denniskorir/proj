fa47b6215ca463995d3ad9c3d994be7289bde5f6
denniskorir <dkdenniskorir@gmail.com>
2017-03-25 15:14:04 +0300

    done

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import {Hello} from "./components/Hello";
 
+ReactDOM.render(
+    <Hello compiler="TypeScript" framework="React"/>,
+    document.getElementById("example")
+);
